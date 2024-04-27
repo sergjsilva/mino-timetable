024ce5b0226d74384d69ac3c157e6dd6cdb74963
Sergio Silva <sergjsilva@gmail.com>
2024-04-27 15:02:02 +0200

    Delete saturday.js

--- saturday.js
+++
@@ -1,7 +0,0 @@
-import { fillTableDataFerrolCoruna } from "./main.js";
-import {
-  ListOfStopsFerrolCoruna,
-  FerrolCorunaSaturdayData,
-} from "./db/busesRoute.js";
-
-fillTableDataFerrolCoruna(ListOfStopsFerrolCoruna, FerrolCorunaSaturdayData);
