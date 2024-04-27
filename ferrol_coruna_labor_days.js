f9baf2dd89601f401b202fb4ca031100bc77215b
Sergio Silva <sergjsilva@gmail.com>
2024-04-27 15:03:31 +0200

    Delete ferrol_coruna_labor_days.js

--- ferrol_coruna_labor_days.js
+++
@@ -1,7 +0,0 @@
-import { fillTableDataFerrolCoruna } from "./main.js";
-import {
-  ListOfStopsFerrolCoruna,
-  FerrolCorunaLaborDaysData,
-} from "./db/busesRoute.js";
-
-fillTableDataFerrolCoruna(ListOfStopsFerrolCoruna, FerrolCorunaLaborDaysData);
