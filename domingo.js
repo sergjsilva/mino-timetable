211c8c3ad7b1ebf49baa66831b6bb1969e50b69e
Sergio Silva <sergjsilva@gmail.com>
2024-04-27 15:04:10 +0200

    Delete domingo.js

--- domingo.js
+++
@@ -1,7 +0,0 @@
-import { fillTableDataFerrolCoruna } from "./main.js";
-import {
-  ListOfStopsFerrolCoruna,
-  FerrolCorunaSundayData,
-} from "./db/busesRoute.js";
-
-fillTableDataFerrolCoruna(ListOfStopsFerrolCoruna, FerrolCorunaSundayData);
